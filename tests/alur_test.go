ac60adde71f62c77f5b48957d24f042bc10d9426
juragan360 <ariefda@hotmail.com>
2016-03-08 17:31:41 +0700

    update test by reading data from context

--- tests/alur_test.go
+++ tests/alur_test.go
@@ -37,14 +37,12 @@ func TestManageStep(t *testing.T) {
 	s2.StepID = "Validate"
 	s2.StepType = alur.StepAction
 	s2.Pre = func(ctx *alur.Context) {
-		m := ctx.Step.Data.(toolkit.M)
-		if m.GetInt("leaveday") <= 0 {
+		if ctx.Request.Data().GetInt("leaveday") <= 0 {
 			ctx.Request.Stop(alur.RequestCancelled, "Admin", "Leave Day can't be 0")
 		}
 	}
 	s2.Exec = func(ctx *alur.Context) {
-		m := ctx.Step.Data.(toolkit.M)
-		dept := m.GetString("department")
+		dept := ctx.Request.Data().GetString("department")
 		if dept == "it" {
 			ctx.Request.StepById("approval").SetApprover(0, "andiek.suncahyo").SetMinimalApprover(0, 1)
 		}
